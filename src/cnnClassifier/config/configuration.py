b934a5f86b529538696998ca927c3427a4d33fdd
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-30 21:50:03 +0530

    callbacks added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,5 +1,5 @@
 from cnnClassifier.constants import *
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, CallbacksConfig
 from cnnClassifier.utils.common import read_yaml, create_directories
 
 
@@ -47,3 +47,29 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+    def get_callback_config(self) -> CallbacksConfig:
+        config = self.config.callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+        param_config = self.params
+
+        callback_config = CallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
+            batch_size=param_config.batch_size,  # set batch size for training
+            epochs=param_config.epochs,  # number of all epochs in training
+            patience=param_config.patience,  # number of epochs to wait to adjust lr if monitored value does not improve
+            stop_patience=param_config.stop_patience,
+            # number of epochs to wait before stopping training if monitored value does not improve
+            threshold=param_config.threshold,
+            # if train accuracy is < threshold adjust monitor accuracy, else monitor validation loss
+            factor=param_config.factor,  # factor to reduce lr by
+            ask_epoch=param_config.ask_epoch,  # number of epochs to run before asking if you want to halt training
+        )
+
+        return callback_config
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -18,3 +18,18 @@ class PrepareBaseModelConfig:
     updated_base_model_path: Path
     params_image_size: list
     params_learning_rate: float
+
+
+@dataclass(frozen=True)
+class CallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
+
+    batch_size: int  # set batch size for training
+    epochs: int  # number of all epochs in training
+    patience: int  # number of epochs to wait to adjust lr if monitored value does not improve
+    stop_patience: int  # number of epochs to wait before stopping training if monitored value does not improve
+    threshold: float  # if train accuracy is < threshold adjust monitor accuracy, else monitor validation loss
+    factor: float  # factor to reduce lr by
+    ask_epoch: int
