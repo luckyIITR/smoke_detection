271b8ff9bf47d14e96787f7212b0c804e5a5b6a7
Lucky Kushwaha <luckykushwaha56@gmail.com>
2024-01-15 15:31:00 +0530

    api url updated

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-    baseURL: 'http://10.14.0.57:10000/',
+    baseURL: 'https://smoke-detection-api.onrender.com/',
     headers: {
         'accept': 'application/json',
         'Content-Type': 'multipart/form-data'
